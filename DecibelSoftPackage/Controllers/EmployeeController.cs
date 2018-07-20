06b004250e41b2eba4d11cbe9c9cbaeca972f4b0
Niranjan.Gangula <Niranjan.Gangula@NGANGULA>
2018-07-20 14:45:51 +1000

    Add project files.

---
+++ DecibelSoftPackage/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DecibelSoftPackage.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Employee
+        public ActionResult EmployeePortal()
+        {
+            return View();
+        }
+    }
+}
---
+++ DecibelSoftPackage/Controllers/GeneralSettingsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DecibelSoftPackage.Controllers
+{
+    public class GeneralSettingsController : Controller
+    {
+        // GET: GeneralSettings
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: GeneralSettings
+        public ActionResult EmployerSettings()
+        {
+            return View();
+        }
+
+    }
+}
